883aaf9100ab5c139a88118cca662bc78988c468
WoocashRwdi <lukaryz444@student.polsl.pl>
2021-11-22 22:25:21 +0100

    Artur M., Łukasz R. - realizacja zadań, patrz arkusz (główne elementy widoku)

---
+++ io-Dorobek/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace io_Dorobek.ViewModel
+{
+    class MainViewModel
+    {
+        private string[] pozycje = { "blablabla blabla 123456789", "blablabla blabla 123456789 ", "blablabla blabla 123456789 ", "blablabla blabla 123456789 ", "blablabla blabla 123456789 ", "blablabla blabla 123456789 ", "blablabla blabla 123456789 ", "blablabla blabla 123456789 ", "blablabla blabla 123456789 " };
+        public string[] Pozycje
+        {
+            get { return pozycje; }
+            private set
+            {
+                for (int i = 0; i < 9; i++)
+                { pozycje[i] = value[i]; }
+            }
+        }
+
+
+
+    }
+}
